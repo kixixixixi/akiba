4403276d73829726775360f2527dd86780a225d0
kixixixixi <kixixixixi@gmail.com>
2025-07-08 21:18:29 +0900

    Change meta inforamtion

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: "Page title",
-  description: "Page description",
+  title: "今日はどこで飲む？",
+  description: "今日はどこで飲む？秋葉原の飲み屋を紹介します。",
 }
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
